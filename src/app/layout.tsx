863024bdd69d40001cab12a3e7ee6e155626d9a8
maybesabin <highsabin987@gmail.com>
2025-08-25 11:43:14 +0545

    SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,14 +11,52 @@ const poppins = Poppins({
   ],
 });
 
-export const metadata: Metadata = {
-  title: "Meme Quick",
-  description: "Quickly generate memes.",
+export const metadata = {
+  title: "MemeQuick - Make Memes in Seconds | Meme Generator",
+  description:
+    "Create hilarious memes in seconds with MemeQuick! Upload images, add top & bottom text, stickers, and share your memes instantly.",
+  keywords: [
+    "Meme generator",
+    "MemeQuick",
+    "Make memes",
+    "Online meme maker",
+    "Funny memes",
+    "Meme editor",
+    "Create memes",
+    "Meme templates",
+    "Meme creator",
+    "Instant memes",
+  ],
   icons: {
     icon: emoji.src,
     shortcut: emoji.src,
     apple: emoji.src
-  }
+  },
+  authors: [{ name: "Sabin Hamal" }],
+  openGraph: {
+    title: "MemeQuick - Make Memes in Seconds | Meme Generator",
+    description:
+      "Create hilarious memes in seconds with MemeQuick! Upload images, add top & bottom text, stickers, and share your memes instantly.",
+    url: "https://meme-quick.vercel.app/",
+    siteName: "MemeQuick",
+    images: [
+      {
+        url: emoji,
+        width: 1200,
+        height: 630,
+        alt: "MemeQuick",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "MemeQuick",
+    title: "MemeQuick - Make Memes in Seconds | Meme Generator",
+    description:
+      "Create hilarious memes in seconds with MemeQuick! Upload images, add top & bottom text, stickers, and share your memes instantly.",
+    images: [emoji],
+  },
 };
 
 export default function RootLayout({
