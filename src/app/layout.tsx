12142a3c09c2ce449e6e07983ea3cb9b3c8af9a5
Sabin Hamal <125813426+maybesabin@users.noreply.github.com>
2025-08-31 20:23:42 +0545

    google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,6 +61,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+      <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-BK37C17YYF"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-BK37C17YYF');
+          `}
+        </Script>
+      </head>
       <body
         className={`${poppins.variable} antialiased`}
       >
